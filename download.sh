15605d243e25287fa04fd79b33ab34ae1e185c78
isksss <104404522+isksss@users.noreply.github.com>
2023-03-31 03:20:11 +0900

    fix

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Download the latest version of the script
+# PaperMC
+URL='https://api.papermc.io/v2/projects/paper'
+# Get the latest version
+VERSION=$(curl -X 'GET' $URL -H 'accept: application/json' | jq -r '.versions[-1]')
+echo "Latest version: $VERSION"
+
+# Get the latest build
+BUILD=$(curl -X 'GET' $URL/versions/$VERSION -H 'accept: application/json' | jq -r '.builds[-1]')
+echo "Latest build: $BUILD"
+
+# Download the jar
+curl -X 'GET' $URL/versions/$VERSION/builds/$BUILD/downloads/paper-$VERSION-$BUILD.jar -o paper.jar
